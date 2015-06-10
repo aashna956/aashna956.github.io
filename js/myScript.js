261f9f47c6f743b938b2f5bbc326dd0367cd64d0
aashna956 <makkar2@illinois.edu>
2015-06-10 14:03:42 -0500

    check

--- js/myScript.js
+++ js/myScript.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 	$('.intro-text').animate({opacity:1},4000);
-	
+
 	$(".uiuc-tile").hover(function() {
-	$(".uiuc-tile").attr("src","img/uiuc2hover.jpg");
+	$(".uiuc-tile").attr("src","img/uiuc2HOVER.jpg");
 	},function()
 	{
 	$(".uiuc-tile").attr("src","img/uiuc22.jpg");
 	});
 	$(".about-me-tile").hover(function() {
-	$(".about-me-tile").attr("src","img/hov1.jpg");
+	$(".about-me-tile").attr("src","img/HOV1.jpg");
 	},function()
 	{
 	$(".about-me-tile").attr("src","img/aboutmetile.jpg");
@@ -19,34 +19,27 @@ $(document).ready(function(){
 	{
 	$(".interests-tile").attr("src","img/gemstile.jpg");
 	});
-	
+
 	$(".link-one").click(function(){
-	 	
+
         $('.uiucHalf').fadeOut(500);
 		$('.interestsHalf').fadeOut(500);
 		$('.aboutMeHalf').delay(500).fadeIn(500);
-         
-    
+
+
 	});
 	$(".link-two").click(function(){
-	 	
+
         $('.aboutMeHalf').fadeOut(500);
 		$('.interestsHalf').fadeOut(500);
 		$('.uiucHalf').delay(500).fadeIn(500);
 	});
 	$(".link-three").click(function(){
-	 	
+
 	 	$('.aboutMeHalf').fadeOut(500);
 		$('.uiucHalf').fadeOut(500);
 		$('.interestsHalf').delay(500).fadeIn(500);
-	 
+
 	 });
-	
+
 });
-
-
-
-
-
-
-
